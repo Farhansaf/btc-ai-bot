9802cc8ed735f59bccdf2495315b4c1b441f975b
Farhansaf <100095970+Farhansaf@users.noreply.github.com>
2026-06-01 18:47:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,27 @@
 import requests
-import json
-import os
-
-FILE = "last_signal.txt"
 
 def get_btc_price():
     url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-    data = requests.get(url).json()
-    return float(data["price"])
+    
+    try:
+        response = requests.get(url, timeout=10)
+        data = response.json()
 
-def generate_signal(price):
-    if price % 2 == 0:
-        return "BUY"
-    else:
-        return "SELL"
+        print("DEBUG RESPONSE:", data)
+
+        if "price" not in data:
+            return None
+
+        return float(data["price"])
+
+    except Exception as e:
+        print("ERROR:", e)
+        return None
+
 
 price = get_btc_price()
-signal = generate_signal(price)
 
-print("BTC Price:", price)
-print("Signal:", signal)
-
-# last signal store karo
-last = ""
-if os.path.exists(FILE):
-    with open(FILE, "r") as f:
-        last = f.read()
-
-# agar signal change hua to update
-if signal != last:
-    print("📢 NEW SIGNAL DETECTED!")
-    with open(FILE, "w") as f:
-        f.write(signal)
+if price:
+    print("BTC Price:", price)
 else:
-    print("No change in signal")
+    print("Price fetch failed")
