8b6284362a959a83a1e5fd9f79f6063f25d8d5b6
Farhansaf <100095970+Farhansaf@users.noreply.github.com>
2026-06-01 18:42:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("BTC AI Bot is running...")
+import requests
+import json
+import os
+
+FILE = "last_signal.txt"
+
+def get_btc_price():
+    url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+    data = requests.get(url).json()
+    return float(data["price"])
+
+def generate_signal(price):
+    if price % 2 == 0:
+        return "BUY"
+    else:
+        return "SELL"
+
+price = get_btc_price()
+signal = generate_signal(price)
+
+print("BTC Price:", price)
+print("Signal:", signal)
+
+# last signal store karo
+last = ""
+if os.path.exists(FILE):
+    with open(FILE, "r") as f:
+        last = f.read()
+
+# agar signal change hua to update
+if signal != last:
+    print("📢 NEW SIGNAL DETECTED!")
+    with open(FILE, "w") as f:
+        f.write(signal)
+else:
+    print("No change in signal")
